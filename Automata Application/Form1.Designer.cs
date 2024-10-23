5aaa522adb778455cb7b8bed4694f84e67a0d55e
Jether Omictin <jethomictin@gmail.com>
2024-10-23 11:44:06 +0800

    initial commit

---
+++ Automata Application/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Automata_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(600, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Visualizer";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(369, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Q0";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(540, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Q1";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(894, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Q3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(496, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "a";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(670, 257);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "b";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(393, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "b";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(921, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "a,b";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(574, 169);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(18, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "a";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(819, 420);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(100, 40);
+            this.btnValidate.TabIndex = 10;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(488, 428);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(318, 26);
+            this.input.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(714, 238);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "Q2";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(842, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "b";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(670, 189);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(18, 20);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "a";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(484, 499);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(435, 39);
+            this.label10.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 615);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Automata Application/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace Automata_Application
+{
+    public partial class Form1 : Form
+    {
+
+        private Dictionary<int, Dictionary<char, int>> transitionTable;
+        private int currentState = 0;
+
+
+
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            transitionTable = new Dictionary<int, Dictionary<char, int>>
+            {
+                { 0, new Dictionary<char, int> { { 'a', 1 }, { 'b', 0 } } },  
+                { 1, new Dictionary<char, int> { { 'a', 1 }, { 'b', 2 } } },  
+                { 2, new Dictionary<char, int> { { 'a', 1 }, { 'b', 3 } } },
+                { 3, new Dictionary<char, int> { { 'a', 3 }, { 'b', 3} } },   
+               
+            };
+
+         
+
+            Button circularButton = button1;
+            Button circularButton2 = button2;
+            Button circularButton3 = button3;
+            Button circularButton4 = button4;
+
+
+
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.FlatAppearance.BorderSize = 0;
+
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(0, 0, circularButton.Width, circularButton.Height);
+            circularButton.Region = new Region(path);
+
+            System.Drawing.Drawing2D.GraphicsPath path2 = new System.Drawing.Drawing2D.GraphicsPath();
+            path2.AddEllipse(0, 0, circularButton2.Width, circularButton2.Height);
+            circularButton2.Region = new Region(path2);
+
+            System.Drawing.Drawing2D.GraphicsPath path3 = new System.Drawing.Drawing2D.GraphicsPath();
+            path3.AddEllipse(0, 0, circularButton3.Width, circularButton3.Height);
+            circularButton3.Region = new Region(path3);
+
+
+            System.Drawing.Drawing2D.GraphicsPath path4 = new System.Drawing.Drawing2D.GraphicsPath();
+            path4.AddEllipse(0, 0, circularButton4.Width, circularButton4.Height);
+            circularButton4.Region = new Region(path4);
+
+
+
+        }
+
+
+        private async Task switchStateAnimation(int state)
+        {
+            
+
+            switch (state)
+            {
+                case 0:
+                    button1.BackColor = Color.LightBlue; // Q0
+                    break;
+                case 1:
+                    button2.BackColor = Color.LightBlue; // Q1
+                    break;
+                case 2:
+                    button4.BackColor = Color.LightBlue; // Q2
+                    break;
+                case 3:
+                    button3.BackColor = Color.LightBlue; // Q3
+                    break;
+                   
+            }
+
+           
+            await Task.Delay(1500);
+
+          
+        }
+
+        public void resetBtnColor()
+        {
+            button1.BackColor = Color.LightCoral;
+            button2.BackColor = Color.LightCoral;
+            button4.BackColor = Color.LightCoral;
+            button3.BackColor = Color.Lime;
+           
+        }
+
+        private async void checker(string regex)
+        {
+            int state = 0; // Starting at initial state Q0
+
+            // Process each character in the input string using the transition table
+
+            foreach (char ch in regex)
+            {
+
+               
+                if (transitionTable[state].ContainsKey(ch))
+                {
+                    
+
+                    state = transitionTable[state][ch];
+                 
+                    
+                    await switchStateAnimation(state); 
+                    resetBtnColor();
+                    Console.WriteLine($"Input: {ch}");
+                    label10.Text = "State: " + state;
+                    Console.WriteLine($"State: Q{state}");
+                }
+                
+                if (state == 3) 
+                {
+                    break;
+                }
+            }
+
+          
+            if (state == 3) 
+            {
+                MessageBox.Show("The string is accepted.");
+            }
+            else
+            {
+                MessageBox.Show("The string is not accepted.");
+            }
+        }
+
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Color.Black, 2); // Define the color and thickness of the line
+
+            // Get the center positions of each button
+            Point button1Center = new Point(button1.Left + button1.Width / 2, button1.Top + button1.Height / 2);
+            Point button2Center = new Point(button2.Left + button2.Width / 2, button2.Top + button2.Height / 2);
+            Point button3Center = new Point(button3.Left + button3.Width / 2, button3.Top + button3.Height / 2);
+            Point button4Center = new Point(button4.Left + button4.Width / 2, button4.Top + button4.Height / 2);
+
+            // Draw straight lines between buttons for transitions
+            g.DrawLine(pen, button1Center, button2Center); // Transition from button1 to button2
+            DrawArrow(g, pen, button1Center, button2Center); // Add arrowhead
+
+            g.DrawLine(pen, button2Center, button3Center); // Transition from button2 to button3
+            DrawArrow(g, pen, button2Center, button3Center); // Add arrowhead
+
+            // Draw loops around Q1, Q2, and Q3
+            Rectangle loopRectQ1 = new Rectangle(button1.Left, button1.Top - 30, 40, 40);
+            Rectangle loopRectQ2 = new Rectangle(button2.Left, button2.Top - 30, 40, 40);
+            Rectangle loopRectQ3 = new Rectangle(button3.Left, button3.Top - 30, 40, 40);
+
+            g.DrawArc(pen, loopRectQ1, 0, 360);
+            DrawLoopArrow(g, pen, loopRectQ1, 270); // Arrowhead for loop around Q1
+
+            g.DrawArc(pen, loopRectQ2, 0, 360);
+            DrawLoopArrow(g, pen, loopRectQ2, 270); // Arrowhead for loop around Q2
+
+            g.DrawArc(pen, loopRectQ3, 0, 360);
+            DrawLoopArrow(g, pen, loopRectQ3, 270); // Arrowhead for loop around Q3
+
+            // Draw a curved line (Bezier curve) from Q2 (button2) to Q1 (button1)
+            Point controlPoint1 = new Point(button4Center.X - 50, button2Center.Y - 50);
+            Point controlPoint2 = new Point(button2Center.X + 50, button2Center.Y - 50);
+            g.DrawBezier(pen, button4Center, controlPoint1, controlPoint2, button2Center);
+            DrawArrow(g, pen, controlPoint2, button2Center); // Arrowhead for curved line
+
+            pen.Dispose();
+        }
+
+        private void DrawArrow(Graphics g, Pen pen, Point start, Point end)
+        {
+            const int arrowSize = 7;
+            float angle = (float)Math.Atan2(end.Y - start.Y, end.X - start.X);
+            PointF arrowPoint1 = new PointF(
+                end.X - arrowSize * (float)Math.Cos(angle - Math.PI / 6),
+                end.Y - arrowSize * (float)Math.Sin(angle - Math.PI / 6));
+            PointF arrowPoint2 = new PointF(
+                end.X - arrowSize * (float)Math.Cos(angle + Math.PI / 6),
+                end.Y - arrowSize * (float)Math.Sin(angle + Math.PI / 6));
+            g.DrawLine(pen, end, arrowPoint1);
+            g.DrawLine(pen, end, arrowPoint2);
+        }
+
+        private void DrawLoopArrow(Graphics g, Pen pen, Rectangle loopRect, int angle)
+        {
+            const int arrowSize = 5;
+            float arrowAngle = angle * (float)Math.PI / 500;
+            PointF arrowPoint = new PointF(
+                loopRect.Right - 3,
+                loopRect.Top + 25
+            ); 
+
+            PointF arrowPoint1 = new PointF(
+                arrowPoint.X - arrowSize * (float)Math.Cos(arrowAngle - Math.PI / 6),
+                arrowPoint.Y - arrowSize * (float)Math.Sin(arrowAngle - Math.PI / 6)
+            );
+            PointF arrowPoint2 = new PointF(
+                arrowPoint.X - arrowSize * (float)Math.Cos(arrowAngle + Math.PI / 6),
+                arrowPoint.Y - arrowSize * (float)Math.Sin(arrowAngle + Math.PI / 6)
+            );
+
+            g.DrawLine(pen, arrowPoint, arrowPoint1);
+            g.DrawLine(pen, arrowPoint, arrowPoint2);
+        }
+
+
+
+
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            string str = input.Text;
+            checker(str);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
